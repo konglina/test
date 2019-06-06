5b239f5f68a452614b3d92429bde12b143d04f9c
konglina <836534675@qq.com>
2019-06-06 09:48:38 +0800

    修改输出

--- test/src/test/me.java
+++ test/src/test/me.java
@@ -3,6 +3,7 @@ package test;
 public class me {
 	public static void main(String[] args) {
 		System.out.println("konglina");
-		System.out.println("liumanyi");
+		System.out.println("liumanyi_konglina");
+		
 	}
 }
